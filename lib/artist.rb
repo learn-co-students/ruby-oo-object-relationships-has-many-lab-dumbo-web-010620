c5d262a12e144cd4202bf9125df92e51600f6b8c
Mari <demarius.edwards@gmail.com>
2020-01-13 20:25:05 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,33 @@
+require_relative 'song.rb'
+class Artist
+    attr_accessor :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def songs
+        Song.all
+    end
+
+    def add_song(song)
+        song.artist = self
+    end
+
+    def add_song_by_name(song_name)
+        new_song = Song.new(song_name)
+        new_song.artist = self
+    end
+    #binding.pry
+
+    def self.song_count
+        Song.all.count
+    end
+
+    def self.all
+        @@all
+    end
+
+
+end
